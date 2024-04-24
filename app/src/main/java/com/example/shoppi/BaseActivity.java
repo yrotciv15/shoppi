606d1871946e227a3c9bb2091a73baa9c57e963e
yrotciv15 <ThanhDK.B20AT175@stu.ptit.edu.vn>
2024-04-24 19:08:13 +0700

    24/04 Commit

---
+++ app/src/main/java/com/example/shoppi/BaseActivity.java
@@ -0,0 +1,21 @@
+package com.example.shoppi;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class BaseActivity extends AppCompatActivity {
+    FirebaseDatabase database;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        database = FirebaseDatabase.getInstance();
+    }
+
+}
